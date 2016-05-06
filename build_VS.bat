106acb8a6675c446e8124631693cec875e1eb4a8
Omegapol <matrac7@gmail.com>
2016-05-07 00:07:05 +0200

    Poprawka by sie kompilowalo pod linuksa i skrypty
    uzyj run-dubug.bat by program sie wykonal i wyswietlil zawartosc konsoli
    bez breakpointa

---
+++ build_VS.bat
@@ -0,0 +1,10 @@
+del /Q log_debug.txt
+set path="C:\Program Files (x86)\MSBuild\14.0\Bin\"
+cd ..
+cls
+MSBuild Container.sln
+start "Containers" /B /W x64\Debug\Kontener.exe > log_debug.txt 2>&1
+echo Done
+start log_debug.txt
+RD Containers\x64 /Q /S
+PAUSE
---
+++ run-debug.bat
@@ -0,0 +1,6 @@
+del /Q log_debug.txt
+cls
+start "Containers" /B /W ../Debug/Kontener.exe > log_debug.txt 2>&1
+echo Done
+start log_debug.txt
+EXIT
