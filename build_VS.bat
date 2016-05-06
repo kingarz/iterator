9c57ae577f6596d8298517090cab6fc7826829d5
Omegapol <matrac7@gmail.com>
2016-05-07 00:38:58 +0200

    naprawione skrypty

--- build_VS.bat
+++ build_VS.bat
@@ -1,4 +1,6 @@
 del /Q log_debug.txt
+::zapisanie oryginalej path
+set o_path=%path%
 set path="C:\Program Files (x86)\MSBuild\14.0\Bin\"
 cd ..
 cls
@@ -6,5 +8,10 @@ MSBuild Container.sln
 start "Containers" /B /W x64\Debug\Kontener.exe > log_debug.txt 2>&1
 echo Done
 start log_debug.txt
+
+::odzyskanie oryginalnej path dla TASKKILL
+set path=%o_path%
+TASKKILL /F /IM vctip.exe
 RD Containers\x64 /Q /S
-PAUSE
+
+exit
